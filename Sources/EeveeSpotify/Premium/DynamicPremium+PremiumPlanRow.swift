f630f5200fd96f8dc62514f7da4af96938cf71c9
pacifichatvr6755-blip <pacific.hat.vr6755@gmail.com>
2026-05-16 11:00:02 -0400

    Update premium plan details from Eevee to Nexus

--- Sources/EeveeSpotify/Premium/DynamicPremium+PremiumPlanRow.swift
+++ Sources/EeveeSpotify/Premium/DynamicPremium+PremiumPlanRow.swift
@@ -2,9 +2,9 @@ import Foundation
 
 func getPremiumPlanBadge() throws -> Data {
     let badge = YourPremiumBadge.with {
-        $0.name = "Eevee"
+        $0.name = "Nexus"
         $0.version = 2
-        $0.colorCode = "#FFD2D7"
+        $0.colorCode = "#609bd1"
     }
     
     return try badge.serializedData()
@@ -13,9 +13,9 @@ func getPremiumPlanBadge() throws -> Data {
 func getPremiumPlanRowData(originalPremiumPlanRow: PremiumPlanRow) throws -> Data {
     var premiumPlanRow = originalPremiumPlanRow
     
-    premiumPlanRow.planName = "EeveeSpotify"
-    premiumPlanRow.planIdentifier = "Eevee"
-    premiumPlanRow.colorCode = "#FFD2D7"
+    premiumPlanRow.planName = "NexusSpotify"
+    premiumPlanRow.planIdentifier = "Nexus"
+    premiumPlanRow.colorCode = "#609bd1"
     
     return try premiumPlanRow.serializedData()
 }
@@ -28,9 +28,9 @@ func getPlanOverviewData() throws -> Data {
         }
         $0.subscription = SpotifyPlan.SubscriptionInfo.with {
             $0.planVariant = 2
-            $0.planName = "EeveeSpotify"
-            $0.planCategory = "Eevee"
-            $0.colorCode = "#FFD2D7"
+            $0.planName = "NexusSpotify"
+            $0.planCategory = "Nexus"
+            $0.colorCode = "#609bd1"
             $0.features = [
                 SpotifyPlan.Feature.with {
                     $0.color = "#1ED760"
